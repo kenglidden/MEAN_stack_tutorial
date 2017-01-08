fc12d48da867d3e57c11edd3ac7d1b898cae4e75
Ken Glidden <kenglidden@gmail.com>
2017-01-08 13:58:04 -0500

    v0.3 Add dummy data in the controller.

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,4 +1,25 @@
 var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("Hello World from controller");
+
+    person1 = {
+      name: 'Tim',
+      email: 'tim@email1.com',
+      number: '111 111 1111'
+    };
+
+    person2 = {
+      name: 'Emily',
+      email: 'Emily@email1.com',
+      number: '222 111 1111'
+    };
+
+    person3 = {
+      name: 'John',
+      email: 'john@email1.com',
+      number: '(333) 333-3333'
+    };
+
+    var contactlist = [person1, person2, person3];
+    $scope.contactlist = contactlist;
 }]);
